95048aecf08cfed21540ac2b75d10736a16dc936
Guilherme Cruz <sambrmg@gmail.com>
2017-08-17 21:46:28 -0300

    Menu com vue.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#nav',
+    data: {
+        itens: [
+            { texto: 'item 1' },
+            { texto: 'item 2' },
+            { texto: 'item 3' }
+        ]
+    }
+});
