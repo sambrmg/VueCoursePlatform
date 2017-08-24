568be08ec2b0e5aa514b8733f2198f3135b61d5a
Guilherme Cruz <sambrmg@gmail.com>
2017-08-23 23:33:40 -0300

    Quiz with new style and add slot to component

--- src/components/chapter/ChapterTwo.vue
+++ src/components/chapter/ChapterTwo.vue
@@ -103,7 +103,9 @@ div.identificador2 p b {
 		 	<p>
 		 		O exemplo acima mostra que somente o <code>&lt;b&gt;</code> dentro da <code>&lt;div&gt;</code> que receberá as regas, como a declaração está assim <code>div.identificador2 p b</code> 'div.identificador2' sem espaço entre eles significa que somente a classe identificador2 que estiver em uma DIV que atenderá as regras.
 			</p>
-      <quiz></quiz>
+      <quiz>
+		  <p>O que é uma variável?</p>
+	  </quiz>
   </div>
 </template>
 
--- src/components/shared/quiz/Quiz.vue
+++ src/components/shared/quiz/Quiz.vue
@@ -1,9 +1,12 @@
 <template>
-    <ol class="answer">
-        <li>Answer 1</li>
-        <li>Answer 2</li>
-        <li>Answer 3</li>
-    </ol>
+    <div class="quiz">
+        <slot></slot>
+        <ol class="answer">
+            <li>Answer 1</li>
+            <li>Answer 2</li>
+            <li>Answer 3</li>
+        </ol>
+    </div>
 </template>
 <script>
 export default {
@@ -11,13 +14,19 @@ export default {
 }
 </script>
 <style >
+    .quiz{
+        padding: 40px;
+        background-color: #fbfbfb;
+        box-sizing: border-box;
+        border: 1px solid #ededed;
+    }
     .answer{
         list-style: none;
         margin: 0;
         padding: 0;
     }
     .answer li {
-        padding: 10px 10px 10px 40px;
+        padding: 20px 10px 20px 60px;
         border: 2px solid #dedede;
         margin-bottom: 10px;
         counter-increment: index;
@@ -30,9 +39,9 @@ export default {
         top: -2px;
         left: -2px;
         bottom: -2px;
-        padding: 7px 10px 10px 10px;
+        padding: 16px 15px 10px 15px;
         background-color: #74a8e3;
         color: white;
-        font-size: 23px;
+        font-size: 28px;
     }
 </style>
