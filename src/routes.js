223624a823a1d73740bdcbfa7e89af8574586453
Guilherme Cruz <sambrmg@gmail.com>
2017-08-28 10:16:17 -0300

    change titulo to title

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import ChapterOne from './components/chapter/ChapterOne.vue';
 import ChapterTwo from './components/chapter/ChapterTwo.vue';
 
 export const routes = [
-    { path: '/', component: ChapterOne, titulo: 'Chapter One'},
-    { path: '/chaptertwo', component: ChapterTwo, titulo: 'Chapter Two'}
+    { path: '/', component: ChapterOne, title: 'Chapter One'},
+    { path: '/chaptertwo', component: ChapterTwo, title: 'Chapter Two'}
 ];
