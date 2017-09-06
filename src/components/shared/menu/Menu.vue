9876b733b60e69b8bab7a755cb09e4a0f06cfe6a
Guilherme Cruz <sambrmg@gmail.com>
2017-09-06 14:58:39 -0300

    Move CSS Menu to component

--- src/components/shared/menu/Menu.vue
+++ src/components/shared/menu/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav>
+    <nav class="main-nav">
       <ul>       
           <li v-for="rota in rotas">
             <router-link :to="rota.path ? rota.path : '/'">
@@ -25,4 +25,52 @@ export default {
 </script>
 
 <style>
+
+.main-nav{
+    width: 260px;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    bottom: 0px;
+    z-index: 99;
+    overflow-y: auto;
+    overflow-x: hidden;
+    background-color: #383e4b;
+    color: black;
+}
+.main-nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+}
+.main-nav ul li a i{
+    margin-right: 10px;
+    color: white;
+    font-size: 17px;
+    position: relative;
+    top: 3px;
+}
+
+.main-nav ul li a{
+    padding: 15px 10px;
+    display: block;
+    color: white;
+    text-decoration: none;
+    font-size: 15px;
+    cursor: pointer;
+}
+
+.main-nav ul li a.completed:after {
+    font-family: "simple-line-icons";
+    content: "\e080";
+    font-weight: bold;
+    color: #34d434;
+    margin-left: 10px;
+}
+
+.main-nav ul li a:hover{
+    background-color: #22262e;
+}
+
 </style>
